72f8ca3e0f0daf8129cc56cf85bfcecf699829b5
Nagaraj Hebbar <79707183+nagaraj2808@users.noreply.github.com>
2024-03-20 12:07:04 +0530

    Delete src directory

--- src/main/java/com/org/employee/employee_management/exceptions/EmployeeAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.org.employee.employee_management.exceptions;
-
-public class EmployeeAlreadyExistsException extends Exception{
-    public EmployeeAlreadyExistsException(String email){
-        super("Employee with email " + email + " already exists");
-    }
-}
--- src/main/java/com/org/employee/employee_management/exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.org.employee.employee_management.exceptions;
-
-public class EmployeeNotFoundException extends Exception{
-    public EmployeeNotFoundException(){
-        super("Employee not found");
-    }
-}
